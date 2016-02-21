b2e8c94586581a391eb01454f2c002cf1a9f36e9
Evan Rumsey <evan.rumsey@gmail.com>
2016-02-21 14:29:19 +1300

    button

---
+++ 2016-Economics/button.js
@@ -0,0 +1,8 @@
+function clicked() {
+	var body = document.querySelector("body")
+	if (body.style.backgroundColor == "purple") {
+		body.style.backgroundColor = "white" 
+	} else{
+		body.style.backgroundColor = "purple"
+	}
+}
